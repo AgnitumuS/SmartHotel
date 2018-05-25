74b01eed1af9692307352940d7020ba8dec4e21e
lingchen-pc <lingchen52@foxmail.com>
2018-05-25 10:04:44 +0800

    添加上海客户服务器地址

--- app/src/main/java/com/wanlong/iptv/utils/Apis.java
+++ app/src/main/java/com/wanlong/iptv/utils/Apis.java
@@ -7,7 +7,7 @@ package com.wanlong.iptv.utils;
 public class Apis {
     public static final String KEY = "76c833fa47e747d5c3bd24241f027eac";
     public static String HEADER = "http://192.168.1.241:9080/";//杭州服务器http://60.191.94.91:9080/
-    public static final String HEADER_ORIGIN = "http://192.168.1.241:9080/";
+    public static final String HEADER_ORIGIN = "http://192.168.1.241:9080/";//上海客户潘俊服务器http://192.168.1.109:9080/
     public static String ROOM_ORIGIN = "501";
     public static final String SETTING_PASSWORD = "56013700";
     ////////////////////////////////////////////////////////////////////////////////////////////////
