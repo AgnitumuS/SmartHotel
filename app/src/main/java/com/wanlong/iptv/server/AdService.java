6112f99022fcdea278b37f67795098008d89c3b0
lingchen-pc <lingchen52@foxmail.com>
2018-06-06 09:54:59 +0800

    添加区域和回看地址参数获取

--- app/src/main/java/com/wanlong/iptv/server/AdService.java
+++ app/src/main/java/com/wanlong/iptv/server/AdService.java
@@ -258,8 +258,10 @@ public class AdService extends Service {
         try {
             editor.putString("group", data.getGroup());
             editor.putString("stb_name", data.getStb_name());
+            editor.putString("area", data.getArea());
             editor.putString("Owner_Group", data.getOwner_Group());
             editor.putString("Owner_Group_display", data.getOwner_Group_display());
+            editor.putString("playback_url", data.getPlayback_url());
             editor.commit();
         } catch (NullPointerException e) {
             e.printStackTrace();
